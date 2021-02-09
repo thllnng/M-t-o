13ac3b5732f7a221ac3b1219f88982fcde308146
thllnng <78812293+thllnng@users.noreply.github.com>
2021-02-10 00:38:42 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    showCity();
+});
+
+
+var villeCourante;
+var nomVilleSelectionnee;
+
+
+function showCity() {
+    // 1. Récupérer la valeur courante du menu 
+    // Pour ça on va faire :
+    let nomVilleSelectionnee = document.getElementById(ville - select).value;
+
+    // 2. Récupérer la liste de toutes les div correspondant à une div de météo 
+    // Pour ça on va faire :
+    let cities = document.getElementsByClassName("city");
+
+    // 3. Parcourir la liste des div et cacher celles qui ne sont pas sélectionnées.
+    // TODO : definir l : la taille max de votre tableau cities
+    for (i = 0; i < 5; i++) {
+        if (cities[i].id == nomVilleSelectionnee) {
+
+            ELEMENT.style.display = "nomVilleSelectionnee"; // garder l'element visible -> ELEMENT.style.display= "none";
+        } else {
+            ELEMENT.style.display = ""; // cacher l'element -> ELEMENT.style.display= "";
+        }
+    }
+}
