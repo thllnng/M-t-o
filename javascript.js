a7202c406070befb0cd8cb212c68081cb48e145c
thllnng <78812293+thllnng@users.noreply.github.com>
2021-02-10 18:52:33 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+let apiCall = (city) => {
+    const apiKey = "bd6b3aa0299986e1043f0b7b23e81ec4";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`;
+
+    fetch(url).then((response) =>
+            response.json().then((data) => {
+                console.log(data)
+                document.querySelector('.temps' + city).innerHTML = data.main.temp;
+            })
+        )
+        .catch((err) => console.log('Erreur :' + err));
+}
+
 function showCity() {
 
     let message = document.querySelector('#label');
@@ -6,7 +19,7 @@ function showCity() {
     let villes = document.getElementsByClassName('city');
     for (let i = 0; i < villes.length; i++) {
         if (nomVilleSelectionnee == villes[i].id) {
-            villes[i].style.display = " ";
+            villes[i].style.display = "";
             villes[i].style.width = "100%";
             message.firstChild.nodeValue = "Vous avez choisi: " + villes[i].id;
 
